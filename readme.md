d1ba33deb8baceabb9c16b544db5740d48c56e86
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-05-29 10:06:17 +0200

    bruh, i'm to lazy to write a proper message. i just did some things, ok?

--- readme.md
+++ readme.md
@@ -1,17 +1,18 @@
 # Smartcard-based web authentication using Python/Flask
 This is a demo server. **It will only work with smart-cards isseud by the Swiss Government.**
+The aim of this demo project is to authenticate users using only their smart-card and their browser against the server without any other 3rd-party software required (except of course the smart-card drivers).
 
-Only minor modifications are necessary for this project to work with other smart-cards:
+Only minor modifications are necessary for this project to work with other (i.e., non-SwissGov) smart-cards:
 
-1. Replace `<project-root>/cert/swissgov-root-ca1.p7b` with the issuing root certificate.
-2. Replace `<project-root>/cert/swissgov-enhanced-ca02.p7b` with the issuing certificate chain. This file may also be identical to `<project-root>/cert/swissgov-root-ca1.p7b`.
-3. Change the following lines in `<project-root>/server.py` if needed:
+1. Replace `<project-root>/cert/root/swissgov-root-ca1.p7b` with the issuing root X.509 certificate.
+2. Replace `<project-root>/cert/root/swissgov-enhanced-ca02.p7b` with the issuing certificate chain. This file may also be identical to `<project-root>/cert/root/swissgov-root-ca1.p7b`.
+3. Change the following lines in `<project-root>/root/server.py` if needed:
     ```diff
-    - CERTIFICATE_CLIENT_ROOT_P7B : str = f'{CERTIFICATE_DIR}/swissgov-root-ca1.p7b'
-    + CERTIFICATE_CLIENT_ROOT_P7B : str = f'{CERTIFICATE_DIR}/your-certificate-root.p7b'
+    - CERTIFICATE_CLIENT_ROOT_P7B : str = f'{CERTIFICATE_ROOT_DIR}/swissgov-root-ca1.p7b'
+    + CERTIFICATE_CLIENT_ROOT_P7B : str = f'{CERTIFICATE_ROOT_DIR}/your-certificate-root.p7b'
     CERTIFICATE_CLIENT_ROOT_PEM : str = CERTIFICATE_CLIENT_ROOT_P7B[:-4] + '.pem'
-    - CERTIFICATE_CLIENT_KEYCHAIN_P7B : str = f'{CERTIFICATE_DIR}/swissgov-enhanced-ca02.p7b'
-    + CERTIFICATE_CLIENT_KEYCHAIN_P7B : str = f'{CERTIFICATE_DIR}/your-certificate-chain.p7b'
+    - CERTIFICATE_CLIENT_KEYCHAIN_P7B : str = f'{CERTIFICATE_ROOT_DIR}/swissgov-enhanced-ca02.p7b'
+    + CERTIFICATE_CLIENT_KEYCHAIN_P7B : str = f'{CERTIFICATE_ROOT_DIR}/your-certificate-chain.p7b'
     CERTIFICATE_CLIENT_KEYCHAIN_PEM : str = CERTIFICATE_CLIENT_KEYCHAIN_P7B[:-4] + '.pem'
     ```
 4. Note that you can also exclusively use the `.pem` format instead of `.p7b`, but that would require also removing the following lines:
@@ -25,22 +26,33 @@ Only minor modifications are necessary for this project to work with other smart
 
 ## Instructions
 
-1. Clone this repo
-2. Run
+1.  Install the smart-card drivers for the cards issued by the Swiss Government. You can find them on [the Thales website](https://cpl.thalesgroup.com/access-management/security-applications/credentialing-safenet-minidriver).
+2.  [Install python](https://www.python.org/downloads/).
+3.  Run the following commands in your terminal:
     ```bash
+    git clone https://github.com/Unknown6656/SmartcardWebAuthTest
     pip install pyopenssl werkzeug flask
+    mkdir cert/user
+    mkdir cert/root
     ```
-3. Run
+4.  Download the Swiss Government X.509 root certificates from https://www.bit.admin.ch/bit/en/home/themes/swiss-government-pki/certificate-service-provider-csp/rootzertifikate/swiss-government-root-ca-i.html and place them in the directory `cert/root`.
+    - `Swiss Government Root CA I`
+    - `Swiss Government Enhanced CA 01`
+    - `Swiss Government Enhanced CA 02`
+5.  Download all X.509 certificates of users you want to "whitelist" from the following pages. Place the user certificates in `cert/user`.
+    - https://admindir.verzeichnisse.admin.ch/ [Swiss Intranet only]
+    - https://staatskalender.admin.ch/ [Public]
+6.  Run the following command in your terminal to start the webserver.
     ```bash
     python server.py
     ```
-4. Navigate to `https://localhost:6996/`
-5. Connect your smart-card issued by the Swiss Government.
-6. Select the smart-card certificate you want to use for the authentication:<br/>
+7.  Navigate to `https://localhost:6996/`
+8.  Connect your smart-card issued by the Swiss Government.
+9.  Select the smart-card certificate you want to use for the authentication:<br/>
     ![](img/screenshot-1.png)
-8. Enter your PIN:<br/>
+10. Enter your PIN:<br/>
     ![](img/screenshot-2.png)
-9. Success.<br/>
+11. Success.<br/>
     ![](img/screenshot-3.png)
-10. If unsuccessful, the following message is displayed:<br/>
+12. If unsuccessful, the following message is displayed:<br/>
     ![](img/screenshot-4.png)
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+import urllib.request
 import typing
+import shutil
 import subprocess
 import sys
 import ssl
@@ -17,9 +19,18 @@ from flask import Flask, request, jsonify, send_file, send_from_directory, redir
 
 CURRENT_DIR : str
 OPENSSL_PATH : str
-
+OPENSSL_SEARCH_LOCATIONS : list[str] = [
+    r"C:\OpenSSL-Win64\bin\openssl.exe",
+    r"C:\OpenSSL-Win32\bin\openssl.exe",
+    r"C:\Program Files\OpenSSL\bin\openssl.exe",
+    r"C:\Program Files (x86)\OpenSSL\bin\openssl.exe",
+    r"C:\Program Files\Git\usr\bin\openssl.exe",
+    r"C:\Program Files (x86)\Git\usr\bin\openssl.exe",
+]
 CERTIFICATE_DIR = 'cert'
-CERTIFICATE_SERVER_PUB_KEY : str = f'{CERTIFICATE_DIR}/server.crt'
+CERTIFICATE_ROOT_DIR : str = f'{CERTIFICATE_DIR}/root'
+CERTIFICATE_CLIENT_DIR : str = f'{CERTIFICATE_DIR}/user'
+CERTIFICATE_SERVER_PUB_KEY : str = f'{CERTIFICATE_DIR}/server.cer'
 CERTIFICATE_SERVER_SEC_KEY : str = f'{CERTIFICATE_DIR}/server.key'
 CERTIFICATE_SERVER_PASSWD = b'018ef1d6-921f-704a-8cd7-54f1f3d415b4' # TODO : change me.
 CERTIFICATE_SERVER_C = 'CH'
@@ -29,14 +40,20 @@ CERTIFICATE_SERVER_O = 'Schweizerische Eidgenossenschaft'
 CERTIFICATE_SERVER_OU = 'VBS'
 CERTIFICATE_SERVER_CN = 'Test'
 CERTIFICATE_SERVER_E = 'info@admin.ch'
-CERTIFICATE_CLIENT_ROOT_P7B : str = f'{CERTIFICATE_DIR}/swissgov-root-ca1.p7b'
-CERTIFICATE_CLIENT_ROOT_PEM : str = CERTIFICATE_CLIENT_ROOT_P7B[:-4] + '.pem'
-CERTIFICATE_CLIENT_KEYCHAIN_P7B : str = f'{CERTIFICATE_DIR}/swissgov-enhanced-ca02.p7b'
-CERTIFICATE_CLIENT_KEYCHAIN_PEM : str = CERTIFICATE_CLIENT_KEYCHAIN_P7B[:-4] + '.pem'
+CERTIFICATE_EXTENSIONS : set[str] = {'.cer', '.pem', '.crt', '.pfx', '.p7b', '.p7c', '.p12', '.key'}
+CERTIFICATE_CLIENT_ROOT : OpenSSL.crypto.X509
+# CERTIFICATE_CLIENT_ROOT_PEM : str = f'{CERTIFICATE_ROOT_DIR}/swissgov-root-ca1.pem'
+# CERTIFICATE_CLIENT_ROOT_DOWNLOAD = 'https://www.bit.admin.ch/dam/bit/de/dokumente/SwissGov-PKI-V2/Root-Zertifikate/swiss-governemt_root_ca-i/swiss_governmentrootcai.crt.download.crt/swiss_governmentrootcai.crt'
+CERTIFICATE_CLIENT_ROOT_PEM : str = f'{CERTIFICATE_ROOT_DIR}/swissgov-enhanced-ca02.pem'
+CERTIFICATE_CLIENT_ROOT_DOWNLOAD = 'https://www.bit.admin.ch/dam/bit/en/dokumente/SwissGov-PKI-V2/Root-Zertifikate/swiss-governemt_root_ca-i/swiss_governmentenhancedca02.cer.download.cer/swiss_governmentenhancedca02.cer'
+CERTIFICATE_CLIENT_ALLOW_ANY = False # Set this to 'True' only for debugging reasons. Set it to 'False' in production!
+CERTIFICATE_CLIENTS : dict[str, str] = { }
+CERTIFICATE_ROOTS : dict[str, str] = { }
 
-# this is only for debugging reasons. set it to 'False' in production
-CERTIFICATE_CLIENT_ALLOW_ANY = False
+# TODO : implement certificate revocation list (CRL) support
 
+
+X509_CERTIFICATE_HEADER = b'-----BEGIN CERTIFICATE-----'
 X509_OID_SERVER_AUTH = b'1.3.6.1.5.5.7.3.1'
 X509_OID_CLIENT_AUTH = b'1.3.6.1.5.5.7.3.2'
 X509_OID_BASIC_CONSTRAINTS = b'2.5.29.19'
@@ -44,21 +61,15 @@ X509_OID_SUBJECT_ALT_NAME = b'2.5.29.17'
 X509_OID_KEY_USAGE = b'2.5.29.15'
 X509_OID_EMPOLYEE_NUMBER = '2.16.840.1.113730.3.1.3'
 
+# make sure to exclude certificate extensions from being served as static files
+WEBSERVER_ALLOWED_STATIC_FILES : set[str] = {'js', 'css', 'html', 'ttf', 'png', 'svg'} - CERTIFICATE_EXTENSIONS
 WEBSERVER_PORT : int = 6996
-ALLOWED_STATIC_FILES : list[str] = ['js', 'css', 'html', 'ttf', 'png', 'svg']
 
 
 
 def find_openssl() -> str:
     try:
-        for path in  [
-            r"C:\OpenSSL-Win64\bin\openssl.exe",
-            r"C:\OpenSSL-Win32\bin\openssl.exe",
-            r"C:\Program Files\OpenSSL\bin\openssl.exe",
-            r"C:\Program Files (x86)\OpenSSL\bin\openssl.exe",
-            r"C:\Program Files\Git\usr\bin\openssl.exe",
-            r"C:\Program Files (x86)\Git\usr\bin\openssl.exe",
-        ]:
+        for path in OPENSSL_SEARCH_LOCATIONS:
             if os.path.exists(path):
                 return path
 
@@ -81,6 +92,40 @@ def convert_p7b_to_pem(p7b_path : str, pem_path) -> None:
 
     with open(pem_path, 'w') as f:
         f.write(pem)
+
+def convert_cer_to_pem(cer_path : str, pem_path) -> None:
+    subprocess.run(
+        [OPENSSL_PATH, 'x509', '-inform', 'DER', '-outform', 'PEM', '-in', cer_path, '-out', pem_path],
+        capture_output = True,
+        text = True,
+        check = True
+    ).stdout.strip()
+
+def ensure_correct_format(pem_path : str) -> bool:
+    if not(os.path.exists(pem_path)):
+        for ext in CERTIFICATE_EXTENSIONS:
+            if os.path.exists(in_path := (pem_path[:-4] + ext)):
+                with open(in_path, 'rb') as f:
+                    if X509_CERTIFICATE_HEADER in f.read():
+                        shutil.copy2(in_path, pem_path)
+                        return True
+
+                if ext == '.cer' or ext == '.crt':
+                    convert_cer_to_pem(in_path, pem_path)
+                elif ext == '.p7b' or ext == '.p7c' or ext == '.p12' or ext == '.pfx':
+                    convert_p7b_to_pem(in_path, pem_path)
+                # TODO : handle .pfx, .key, .der
+                else:
+                    shutil.copy2(in_path, pem_path)
+
+                return True
+        return False
+    return True
+
+def get_x509_name(x509name : OpenSSL.crypto.X509Name) -> dict[str, str]:
+    raw : list[str] = re.search('\'(?P<C>[^\']*)\'', str(x509name).replace('"', '\'')).group('C').split('/')
+
+    return {key: value for key, value in [token.split('=') for token in raw if '=' in token]}
 
 def create_ssl_keypair() -> None:
     pkey = OpenSSL.crypto.PKey()
@@ -122,15 +167,64 @@ def create_ssl_keypair() -> None:
     with open(CERTIFICATE_SERVER_SEC_KEY, 'w') as f:
         f.write(seckey)
 
-def get_x509_name(x509name : OpenSSL.crypto.X509Name) -> dict[str, str]:
-    raw : list[str] = re.search('\'(?P<C>[^\']*)\'', str(x509name).replace('"', '\'')).group('C').split('/')
+def is_certificate_root(cert : OpenSSL.crypto.X509, root : OpenSSL.crypto.X509) -> bool:
+    return False
 
-    return {key: value for key, value in [token.split('=') for token in raw if '=' in token]}
+def load_client_certificates() -> None:
+    for root, _, files in os.walk(CERTIFICATE_CLIENT_DIR):
+        for file in files:
+            for ext in CERTIFICATE_EXTENSIONS:
+                if file.endswith(ext):
+                    if ext == '.p7b':
+                        convert_p7b_to_pem(f'{root}/{file}', f'{root}/{file[:-4]}.pem')
+                        file : str = f'{file[:-4]}.pem'
 
+                    with open(f'{root}/{file}', 'rb') as f:
+                        cert : OpenSSL.crypto.X509 = None
 
-class PeerCertWSGIRequestHandler(werkzeug.serving.WSGIRequestHandler):
+                        for format in [OpenSSL.crypto.FILETYPE_ASN1, OpenSSL.crypto.FILETYPE_PEM, OpenSSL.crypto.FILETYPE_TEXT]:
+                            try:
+                                cert = OpenSSL.crypto.load_certificate(format, f.read())
+                            except:
+                                pass
+
+                        if cert is None:
+                            continue
+
+                        subject : dict[str, str] = get_x509_name(cert.get_subject())
+                        issuer : dict[str, str] = get_x509_name(cert.get_issuer())
+                        serial : str = f'{cert.get_serial_number():032x}'
+
+                        if not is_certificate_root(cert, CERTIFICATE_CLIENT_ROOT_PEM):
+                            print(f'The certificate "{file}" ({serial}, issuer) is not signed by the root certificate "{CERTIFICATE_CLIENT_ROOT_PEM}" and will therefore be ignored.')
+                        else:
+                            CERTIFICATE_CLIENTS[subject['CN']] = {
+                                'cert': cert,
+                                'serial': serial,
+                                'issuer': issuer,
+                                'subject': subject,
+                            }
+
+def verify_client_certificate(cert : OpenSSL.crypto.X509, subject : dict[str, str], issuer : dict[str, str]) -> bool:
+    for cn in CERTIFICATE_CLIENTS:
+        known : dict[str] = CERTIFICATE_CLIENTS[cn]
+        serial : str = f'{cert.get_serial_number():032x}'
+
+        if known['serial'] == serial:
+            return True
+        elif not is_certificate_root(known['cert'], CERTIFICATE_CLIENT_ROOT_PEM):
+            return False
+
+        if known['subject'] == subject and known['issuer'] == issuer:
+            return True
+
+    # TODO : check if cert is valid / not revoked
+
+    return False
+
+class request_handler(werkzeug.serving.WSGIRequestHandler):
     def make_environ(self) -> dict[str, typing.Any]:
-        environ : dict[str, typing.Any] = super(PeerCertWSGIRequestHandler, self).make_environ()
+        environ : dict[str, typing.Any] = super(request_handler, self).make_environ()
         environ['wsgi.url_scheme'] = 'https'
         environ['user_cert'] = None
 
@@ -154,22 +248,16 @@ def route_index():
     subject : dict[str, str] = get_x509_name(cert.get_subject())
     issuer : dict[str, str] = get_x509_name(cert.get_issuer())
 
-    # TODO : check if issuer is CERTIFICATE_CLIENT_ROOT_PEM or CERTIFICATE_CLIENT_KEYCHAIN_PEM
-    # TODO : check if cert is valid / not revoked
-    # TODO : compare subject with database of allowed users
+    if not verify_client_certificate(cert, subject, issuer):
+        return render_template('401-cert.html', cert = cert, issuer = issuer, subject = subject), 401
 
-    return render_template(
-        'success.html',
-        cert = cert,
-        issuer = issuer,
-        subject = subject
-    ), 200
+    return render_template('success.html', cert = cert, issuer = issuer, subject = subject), 200
 
 @app.route('/<path:filename>', methods = ['GET', 'POST'])
 def serve_static_files(filename):
     if len(filename or '') == 0:
         return redirect('/', 301)
-    elif filename.lower()[filename.rfind('.') + 1:] in ALLOWED_STATIC_FILES:
+    elif filename.lower()[filename.rfind('.') + 1:] in WEBSERVER_ALLOWED_STATIC_FILES:
         if request.method == 'GET':
             return send_from_directory('.', filename)
         else:
@@ -181,25 +269,35 @@ def serve_static_files(filename):
 if __name__ == '__main__':
     OPENSSL_PATH = find_openssl() or 'openssl'
 
-    if not os.path.exists(CERTIFICATE_DIR):
-        os.makedirs(CERTIFICATE_DIR)
+    for dir in [CERTIFICATE_DIR, CERTIFICATE_ROOT_DIR, CERTIFICATE_CLIENT_DIR]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    if not ensure_correct_format(CERTIFICATE_CLIENT_ROOT_PEM):
+        try:
+            urllib.request.urlretrieve(CERTIFICATE_CLIENT_ROOT_DOWNLOAD, CERTIFICATE_CLIENT_ROOT_PEM[:-4] + '.cer')
+            ensure_correct_format(CERTIFICATE_CLIENT_ROOT_PEM)
+        except:
+            print(f'Failed to resolve/read root certificate "{CERTIFICATE_CLIENT_ROOT_PEM}" or to download it from "{CERTIFICATE_CLIENT_ROOT_DOWNLOAD}".')
+            exit(-1)
+
+    with open(CERTIFICATE_CLIENT_ROOT_PEM, 'rb') as f:
+        CERTIFICATE_CLIENT_ROOT = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, f.read())
+
+    load_client_certificates()
 
     if not(os.path.exists(CERTIFICATE_SERVER_PUB_KEY) and os.path.exists(CERTIFICATE_SERVER_SEC_KEY)):
         create_ssl_keypair()
 
-    if not(os.path.exists(CERTIFICATE_CLIENT_ROOT_PEM)):
-        convert_p7b_to_pem(CERTIFICATE_CLIENT_ROOT_P7B, CERTIFICATE_CLIENT_ROOT_PEM)
-
-    if not(os.path.exists(CERTIFICATE_CLIENT_KEYCHAIN_PEM)):
-        convert_p7b_to_pem(CERTIFICATE_CLIENT_KEYCHAIN_P7B, CERTIFICATE_CLIENT_KEYCHAIN_PEM)
-
-    sslctx : ssl.SSLContext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cafile = None if CERTIFICATE_CLIENT_ALLOW_ANY else CERTIFICATE_CLIENT_KEYCHAIN_PEM)
+    sslctx : ssl.SSLContext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cafile = None if CERTIFICATE_CLIENT_ALLOW_ANY else CERTIFICATE_CLIENT_ROOT_PEM)
     sslctx.load_cert_chain(CERTIFICATE_SERVER_PUB_KEY, CERTIFICATE_SERVER_SEC_KEY, CERTIFICATE_SERVER_PASSWD)
 
     if not CERTIFICATE_CLIENT_ALLOW_ANY:
         sslctx.load_verify_locations(CERTIFICATE_CLIENT_ROOT_PEM)
+        sslctx.verify_mode = ssl.CERT_REQUIRED
+    else:
+        sslctx.verify_mode = ssl.CERT_OPTIONAL
 
-    sslctx.verify_mode = ssl.CERT_OPTIONAL
     sslctx.check_hostname = False
 
     app.run(
@@ -208,5 +306,5 @@ if __name__ == '__main__':
         debug = False,
         threaded = True,
         ssl_context = sslctx,
-        request_handler = PeerCertWSGIRequestHandler
+        request_handler = request_handler
     )
